d55630d278db0c53a037ca8f94bf2f4c68f5b929
ruuendigital <max@madebyruuen.com>
2023-05-02 09:03:42 +1000

    Partially add validation functionality, needs improvement
    Validation isnt complete and needs slight reworking to function as normal
    
    Currently having problems when the validation occurs and is then cleared, when another incorrect validation occurs it doesn't catch it

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -9,6 +9,11 @@ export default function UserInput({ handleAgeChange }) {
     year: null,
   });
 
+  const [formError, setFormError] = useState({
+    hasError: false,
+    details: [],
+  });
+
   function handleChange(e) {
     const { name, value } = e.target;
 
@@ -20,42 +25,141 @@ export default function UserInput({ handleAgeChange }) {
     });
   }
 
+  function validateFormInputs() {
+    // Update the state to clear any old errors
+    setFormError({
+      hasError: false,
+      details: [],
+    });
+
+    const errors = [];
+    const fieldData = Object.entries(birthday);
+
+    // Individual field value checks
+    fieldData.forEach((field) => {
+      const fieldName = field[0];
+      const value = field[1];
+
+      // Check if field empty
+      if (value == "") {
+        errors.push({
+          field: fieldName,
+          message: "This field is required",
+        });
+
+        return;
+      }
+
+      // Check if field values within correct number range
+      switch (fieldName) {
+        case "day":
+          if (value < 1 || value > 31) {
+            errors.push({
+              field: fieldName,
+              message: "Must be a valid day",
+            });
+          }
+          return;
+        case "month":
+          if (value < 1 || value > 12) {
+            errors.push({
+              field: fieldName,
+              message: "Must be a valid month",
+            });
+          }
+          return;
+        case "year":
+          if (value < 1900)
+            errors.push({
+              field: fieldName,
+              message: "Must be after 1900",
+            });
+          return;
+        default:
+          break;
+      }
+    });
+
+    // Check if provided input values make a valid date
+    let date;
+    try {
+      date = DateTime.fromObject({
+        year: birthday.year,
+        month: birthday.month,
+        day: birthday.day,
+      });
+    } catch (error) {
+      errors.splice(0, errors.length, {
+        field: "day",
+        message: "Must be a valid date",
+      });
+      return;
+    }
+
+    // // Check if provided date is in the past
+    // if (date > DateTime.now()) {
+    //   errors.push({
+    //     fieldName: "year",
+    //     message: "Must be in the past",
+    //   });
+    //   return;
+    // }
+
+    // If there were any errors found, update the associated state
+    if (errors.length == 0) return;
+
+    setFormError({
+      hasError: true,
+      details: errors,
+    });
+
+    return;
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
 
+    validateFormInputs();
+
+    if (formError.hasError) return;
+
     // TODO: add error handling functionality & remove alert placeholders
-    if (birthday.day === "" || birthday.month === "" || birthday.year === "") {
-      alert("Field empty");
-      return;
-    }
+    // if (birthday.day === "" || birthday.month === "" || birthday.year === "") {
+    //   alert("Field empty");
+    //   return;
+    // }
 
-    if (birthday.day < 1 || birthday.day > 31) {
-      alert("Day out of range");
-      return;
-    }
+    // if (birthday.day < 1 || birthday.day > 31) {
+    //   alert("Day out of range");
+    //   return;
+    // }
 
-    if (birthday.month < 1 || birthday.month > 12) {
-      alert("Month out of range");
-      return;
-    }
+    // if (birthday.month < 1 || birthday.month > 12) {
+    //   alert("Month out of range");
+    //   return;
+    // }
 
-    if (birthday.year < 1900) {
-      alert("Year out of range");
-      return;
-    }
+    // if (birthday.year < 1900) {
+    //   alert("Year out of range");
+    //   return;
+    // }
 
-    let date;
-    try {
-      date = DateTime.fromObject({ year: birthday.year, month: birthday.month, day: birthday.day });
-    } catch (error) {
-      alert("Invalid date");
-      return;
-    }
+    // let date;
+    // try {
+    //   date = DateTime.fromObject({
+    //     year: birthday.year,
+    //     month: birthday.month,
+    //     day: birthday.day,
+    //   });
+    // } catch (error) {
+    //   alert("Invalid date");
+    //   return;
+    // }
 
-    if (date > DateTime.now()) {
-      alert("Date in future");
-      return;
-    }
+    // if (date > DateTime.now()) {
+    //   alert("Date in future");
+    //   return;
+    // }
 
     handleAgeChange(birthday);
   }
